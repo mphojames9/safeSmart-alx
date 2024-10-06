021f6c082fa8dad7ac67c8b5f3c6aaceeb995bc9
Mpho Matli <mphojames9@gmail.com>
2024-10-07 00:25:46 +0200

    update

--- index.js
+++ index.js
@@ -43,3 +43,26 @@ signupButton.addEventListener("click", ()=>{
 loginCloseButton.addEventListener("click",()=>{
   loginForm.classList.remove("showForm")
 })
+
+const observer = new IntersectionObserver(
+  (entries)=> {
+entries.forEach((entry) => {
+  if (entry.isIntersecting) {
+    entry.target.classList.add("in-view")
+    entry.target.classList.remove('not-in-view')
+  } else {
+    entry.target.classList.remove("in-view")
+    entry.target.classList.add('not-in-view')
+  } 
+})
+},
+{
+  rootMargin: '0px',
+  threshold: [0, 0.1, 1]
+},)
+
+const tags = document.querySelectorAll('.fade')
+
+tags.forEach((tag) => {
+  observer.observe(tag)
+})
